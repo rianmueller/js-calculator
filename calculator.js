df823f0614f9b9dc69ebfdd47517fafc016a0870
Rian Mueller <rian@Rians-MacBook-Pro-2.local>
2019-06-19 21:20:24 -1000

    completed

--- calculator.js
+++ calculator.js
@@ -6,6 +6,65 @@
  * @return {object} `calculator` object that can be used
  */
 
+function calculatorModule(){
+    let calculator = {};
+    let memory = 0;
+    let total = 0;
+
+    calculator.validation = function(number){
+        if (typeof number !== 'number'){
+            throw 'Error';
+        }
+    };
+
+    calculator.load = function(number){
+        this.validation(number);
+        total = number;
+        return total;
+    };
+
+    calculator.getTotal = function(){
+        return total;
+    };
+
+    calculator.add = function(number){
+        this.validation(number);
+        total = total + number;
+        return total;
+    };
+
+    calculator.subtract = function(number){
+        this.validation(number);
+        total = total - number;
+        return total;
+    };
+
+    calculator.multiply = function(number){
+        this.validation(number);
+        total = total * number;
+        return total;
+    };
+
+    calculator.divide = function(number){
+        this.validation(number);
+        total = total / number;
+        return total;
+    };
+
+    calculator.recallMemory = function(){
+        return memory;
+    };
+
+    calculator.saveMemory = function(){
+        memory = total;
+    };
+
+    calculator.clearMemory = function(){
+        memory = 0;
+    }
+
+    return calculator;
+};
 
   /**
    * sets the `total` to the number passed in
